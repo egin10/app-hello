ad0982c7c740ee5bb4b5e37847ab1f44f01c2ec2
egin10 <egin066@gmail.com>
2022-04-09 16:51:45 +0700

    update module to v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/egin10/app-hello
 
 go 1.18
 
-require github.com/egin10/go-hello v1.0.1
+require github.com/egin10/go-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	go_hello "github.com/egin10/go-hello"
+	go_hello "github.com/egin10/go-hello/v2"
 )
 
 func main() {
-	hello := go_hello.Hello()
+	hello := go_hello.Hello("Ucok")
 	fmt.Println(hello)
 
 	sayHelloTo := go_hello.SayHelloTo("Egin")
